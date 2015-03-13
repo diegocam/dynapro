aaa8db1bab6db2947f30d9d01ee4c6c73931be7d
Diego Camacho <dcamacho@americandiscountcruises.com>
2015-03-13 09:34:24 -0400

    init commits

---
+++ application/views/templates/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Welcome to CodeIgniter</title>
+
+        <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+        <link href="<?= base_url() ?>assets/css/reset.css" rel="stylesheet">
+        <link href="<?= base_url() ?>assets/css/main.css" rel="stylesheet">
+
+    </head>
+    <body>
+        <div id="top_nav">
+            <div id="nav_table">
+                <span id="quick_contact">
+                    <a href="">Quick Contact</a>
+                </span>
+                <span id="contact_us">
+                    <img src="http://www.dynaprodirect.com/wp-content/uploads/2014/10/TopButton.png">
+                </span>
+                <div class="clear_float"></div>
+            </div>
+        </div>
+        <div id="header">
+            <div id="logo">
+                <a href="<?= base_url() ?>">
+                    <img src="http://dynaprodirect.com/wp-content/uploads/2014/10/dynapro-logo.jpg">
+                </a>
+            </div>
+            <div id="navigaton">
+                <nav>
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">About</a></li>
+                        <li><a href="">Resistance Bands</a></li>
+                        <li><a href="">Workouts</a></li>
+                        <li><a href="">Prograde vs. Standard</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <div id="main_container">
